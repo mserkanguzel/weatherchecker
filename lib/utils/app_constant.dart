cfd170343015e2b69d8d592b576c743c1b2afbe0
mehmet serkan <mserkanguzel@gmail.com>
2024-04-15 17:01:01 +0300

    Added app constant class

---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,5 @@
+class AppConstant {
+  static String appName = "Weather Checker";
+  static String weatherAPIKey = "";
+  static String weatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+}
